7718b17f590e649e7173c0a7a0b2e3ed97abdd2b
Jack Doe <spasov@droneskye.com>
2017-07-28 15:36:53 +0300

    Application ready. You can predict the future now.

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,11 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import java.util.LinkedList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView ballDisplay = (ImageView) findViewById(R.id.image_eightBall);
+
+        final LinkedList<Integer> ballArray = new LinkedList<>();
+        ballArray.add(R.drawable.ball1);
+        ballArray.add(R.drawable.ball2);
+        ballArray.add(R.drawable.ball3);
+        ballArray.add(R.drawable.ball4);
+        ballArray.add(R.drawable.ball5);
+
+        Button myButton = (Button)findViewById(R.id.askButton);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random randomNumberGenerator = new Random();
+               final int number = randomNumberGenerator.nextInt(5);
+
+                ballDisplay.setImageResource(ballArray.get(number));
+            }
+        });
     }
 }
